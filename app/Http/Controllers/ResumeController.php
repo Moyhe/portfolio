bb8569548360c74fdc369603c98f6733b589682e
Mohye <dragonsinmeliodas777@gmail.com>
2024-09-17 02:24:44 +0300

    configure deployment files

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -10,12 +10,12 @@ class ResumeController extends Controller
 {
     public function __invoke()
     {
-        $fileName = 'resume/' . 'Mohye_Mahmoud_Full_Stack_Laravel_React_Developer.pdf';
+        $fileName = 'resume/Mohye_Mahmoud_Full_Stack_Laravel_React_Developer.pdf';
 
-        if (! Storage::exists($fileName)) {
+        if (! Storage::disk('public')->exists($fileName)) {
             abort(404, 'Resume Not Found');
         }
 
-        return Storage::download($fileName);
+        return Storage::disk('public')->download($fileName);
     }
 }
